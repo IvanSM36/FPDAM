f0f271d441b0a51f660b515d0011fafac3293115
IvanSM <ivan_sm@hotmail.es>
2023-01-11 13:20:23 +0100

    Ejercicio02 terminado

--- Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion04_IvanSM/Ejercicio01_IvanSM/Ejercicio01_IvanSM/MainWindow.xaml.cs
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion04_IvanSM/Ejercicio01_IvanSM/Ejercicio01_IvanSM/MainWindow.xaml.cs
@@ -55,7 +55,5 @@ namespace Ejercicio01_IvanSM
 
         }
 
-
-
     }
 }
--- Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion04_IvanSM/Ejercicio02_IvanSM/Ejercicio02_IvanSM/MainWindow.xaml.cs
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion04_IvanSM/Ejercicio02_IvanSM/Ejercicio02_IvanSM/MainWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace Ejercicio02_IvanSM
         {
             InitializeComponent();
         }
+
+        //Contador de intentos
+        private int intentos = 3;
+
+        // Metodo que comprueba la respuesta de la pregunta
+        private void comprobar(object sender, RoutedEventArgs e)
+        {
+            //Comprueba si el radioButton amarillo esta checkeado
+            if (rbPlatano.IsChecked == true)
+            {
+                MessageBox.Show("Correcto.");
+            }
+            else
+            {
+                intentos--;// restamos 1 intento si nos equivocamos
+                MessageBox.Show("Incorrecto," + " Te quedan " + intentos + " intentos");
+
+            }
+
+            //Comprobamos si no nos quedan intentos bloqueamoslos radiobuttons
+            if (intentos == 0)
+            {
+                // Bloqueamos los radioButton
+                rbFresa.IsEnabled = false;
+                rbPlatano.IsEnabled = false;
+                rbArandano.IsEnabled = false;
+                rbPera.IsEnabled = false;
+                MessageBox.Show("No tequedan intentos");
+            }
+
+        }
     }
 }
