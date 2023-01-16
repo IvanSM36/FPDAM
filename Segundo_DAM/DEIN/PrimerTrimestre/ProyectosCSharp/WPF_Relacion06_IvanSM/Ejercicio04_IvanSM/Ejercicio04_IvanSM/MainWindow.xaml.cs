aba3a320ef2e339c50448e6ae23e0b5e8018235b
IvanSM36 <ivan_sm@hotmail.es>
2023-01-16 21:03:33 +0100

    Ejercicio04 empezado

---
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion06_IvanSM/Ejercicio04_IvanSM/Ejercicio04_IvanSM/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio04_IvanSM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+    }
+}
