e8e9c819822e1cddc8712299c5dee8a4dd1a11dd
IvanSM <ivan_sm@hotmail.es>
2023-01-17 10:32:30 +0100

    Ejercicio04 terminada

--- Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion06_IvanSM/Ejercicio04_IvanSM/Ejercicio04_IvanSM/MainWindow.xaml.cs
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion06_IvanSM/Ejercicio04_IvanSM/Ejercicio04_IvanSM/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,11 +28,30 @@ namespace Ejercicio04_IvanSM
             InitializeComponent();
         }
 
-        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        /* *
+         * Metodo para abrir un fichero .txt
+         */
+        private void btnAbrirArchivo(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt"; // Filtra para que solo se pueda abrir archivos .txt
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // Establece el directorio inicial en la carpeta "Escritorio"
+
             if (openFileDialog.ShowDialog() == true)
                 txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        /* *
+         * Metodo que guarda el contenido del textBox en un archivo .txt
+         */
+        private void btnGuardarArchivo(object sender, RoutedEventArgs e)
+        {         
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt";// Hace que solo se pueda guardar con formato .txt
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); // Establece el directorio inicial en la carpeta "Escritorio"
+
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
         }
     }
 }
--- Segundo_DAM/PROGm/AppSQLite/app/src/main/java/com/example/appsqlite/ConexionSQLiteHelper.java
+++ Segundo_DAM/PROGm/AppSQLite/app/src/main/java/com/example/appsqlite/ConexionSQLiteHelper.java
@@ -1,2 +1,25 @@
-package com.example.appsqlite;public class ConexionSQLiteHelper {
+package com.example.appsqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ConexionSQLiteHelper extends SQLiteOpenHelper {
+
+    public ConexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE usuario (id INTEGER, nombre TEXT, telefono TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
 }
