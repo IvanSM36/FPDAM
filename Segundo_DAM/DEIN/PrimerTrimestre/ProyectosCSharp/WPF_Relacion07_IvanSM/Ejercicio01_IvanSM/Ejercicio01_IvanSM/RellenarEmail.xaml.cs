c13275a2ff216f782a178656efe8223673e55fe1
IvanSM <ivan_sm@hotmail.es>
2023-01-19 10:28:39 +0100

    ejercicio 01

---
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion07_IvanSM/Ejercicio01_IvanSM/Ejercicio01_IvanSM/RellenarEmail.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ejercicio01_IvanSM
+{
+    /// <summary>
+    /// Lógica de interacción para RellenarEmail.xaml
+    /// </summary>
+    public partial class RellenarEmail : Window
+    {
+        public RellenarEmail()
+        {
+            InitializeComponent();
+        }
+    }
+}
