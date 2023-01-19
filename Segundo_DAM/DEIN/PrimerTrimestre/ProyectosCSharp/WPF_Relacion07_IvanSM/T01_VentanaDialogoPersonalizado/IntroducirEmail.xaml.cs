be015306764d386efcf2d9a5415268441db49ca2
IvanSM36 <ivan_sm@hotmail.es>
2023-01-19 19:37:00 +0100

    Initial commit

---
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion07_IvanSM/T01_VentanaDialogoPersonalizado/IntroducirEmail.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace T01_VentanaDialogoPersonalizado
+{
+    /// <summary>
+    /// Lógica de interacción para IntroducirEmail.xaml
+    /// </summary>
+    public partial class IntroducirEmail : Window
+    {
+
+        /* /
+         * Constructor sin parametros, para cuando no pidamos una pregunta coja la que hay por defecto en el .xaml
+         * 
+         */
+        public IntroducirEmail()
+        {
+            InitializeComponent();
+        }
+
+        /* *
+         * Constructor que recoge por parametro una pregunta y respuesta para la ventana dialogo
+         */
+        public IntroducirEmail(string question, string defaultRespuesta = "")
+        {
+            InitializeComponent();
+
+            lblQuestion.Content = question;
+
+            txtRespuestaEmail.Text = defaultRespuesta;
+        }
+
+        /* *
+         *  Metodo para el boton aceptar que confirma el email introducido en textBox.
+         */
+        private void dialogAceptarEmail(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        /* *
+         * 
+         */
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            txtRespuestaEmail.SelectAll();
+            txtRespuestaEmail.Focus();
+        }
+
+        /* *
+         * Metodo que recoge el email del textBox y lo guarda.
+         */
+        public string Respuesta
+        {
+            get { return txtRespuestaEmail.Text; }
+        }
+
+    }
+}
---
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion07_IvanSM/T01_VentanaDialogoPersonalizado/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T01_VentanaDialogoPersonalizado
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* * 
+         *  Creo un metodo donde llamamos a la otra ventana y recogemos la respuesta del textTbox email.
+         */
+        private void aniadirEmail(object sender, RoutedEventArgs e)
+        {
+            IntroducirEmail email = new IntroducirEmail();
+
+            if (email.ShowDialog() == true)
+            {
+                txtEmail.Text = email.Respuesta;
+            }
+        }
+    }
+}
