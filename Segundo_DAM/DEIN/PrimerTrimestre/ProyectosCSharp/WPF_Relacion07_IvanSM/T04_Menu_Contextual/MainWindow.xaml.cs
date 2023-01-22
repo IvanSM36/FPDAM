d90c446c06361dcc35f3cff50c92a2bc5161e85c
IvanSM36 <ivan_sm@hotmail.es>
2023-01-22 17:58:22 +0100

    Relacion 07 Terminada

---
+++ Segundo_DAM/DEIN/PrimerTrimestre/ProyectosCSharp/WPF_Relacion07_IvanSM/T04_Menu_Contextual/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T04_Menu_Contextual
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* *
+         * Metodo que abre una ventana message box y muestra el contenido del texto seleccinado
+         */
+        private void eventoMostrar(object sende , RoutedEventArgs e)
+        {
+            String textoSeleccionado = txtBox.SelectedText;
+            MessageBox.Show(textoSeleccionado, "Texto seleccionado" , MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+    }
+}
