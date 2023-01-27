85fe3b844a1174a2f0b0a0076b1bd52b6f2626da
IvanSM <ivan_sm@hotmail.es>
2023-01-27 14:41:26 +0100

    commit

---
+++ Segundo_DAM/DEIN/SegundoTrimestre/WPF_Relacion12_IvanSM/T01_ListView_IvanSM/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace T01_ListView_IvanSM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Creamos una lista de persona
+        List<Persona> lp = new List<Persona>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void agregarPersona(Object sender, RoutedEventArgs e)
+        {
+            lp.Add(new Persona() {Nombre = "Ivan", Apellidos = "Salguero", Dni = "44444444-W", Email= "ivan_sm@hotmail.es", Edad = 26 });
+        }
+    }
+}
